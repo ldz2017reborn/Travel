01172427d117d33448b7344e401c142ba3cdff09
Your Name <you@example.com>
2018-08-14 18:27:04 +0800

    add Icons.vue

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png">
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png">
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png">
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png">
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png">
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png">
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png">
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png">
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+  .icons
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          height: 100%
+          margin: 0 auto
+      .icon-desc
+        position: absolute
+        bottom: 0
+        left: 0
+        right: 0
+        text-align: center
+        line-height: .44rem
+        height: .44rem
+        color: $darkTextColor
+</style>
