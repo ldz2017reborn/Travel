2202525176f368ef0b2c6d0908cab37005a00296
Your Name <you@example.com>
2018-08-16 08:33:52 +0800

    add city-search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search">
+    <input class="input-search" type="text" placeholder="请输入城市名字" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .input-search
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      line-height: .62rem
+      border-radius: .06rem
+      text-align: center
+      padding: 0.1rem
+</style>
