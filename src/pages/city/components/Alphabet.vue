01a38f2c3224735238fc9775474dc4bc0dc62699
Your Name <you@example.com>
2018-08-16 15:33:29 +0800

    use ajax to add json data

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,14 +1,6 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
   </ul>
 </template>
 
@@ -16,6 +8,9 @@
 
 export default {
   name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
